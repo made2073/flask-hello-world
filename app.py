50fa6b193944aecddeef208389c946eccf971895
Matthew DeBusk <matthewdbk11@gmail.com>
2026-07-28 02:39:31 +0000

    Add postgresSQL dependency and db_test route

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+import os
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 @app.route('/')
 def index():
     return 'Hello World from Matthew DeBusk in 3308'
+
+@app.route("/db_test")
+def ddb_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database successfully connected."
+    except Exception as e:
+        return f"Database failed connection: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
